5ad891fce08e2b8cf4140cdd05ab79fc5d921218
Gan Quan <coin2028@hotmail.com>
2018-09-03 17:34:35 -0400

    VAE training draft

--- jtnn/jtnn_vae.py
+++ jtnn/jtnn_vae.py
@@ -90,10 +90,10 @@ class JTNNVAE(nn.Module):
         z_log_var = torch.cat([tree_log_var,mol_log_var], dim=1)
         kl_loss = -0.5 * torch.sum(1.0 + z_log_var - z_mean * z_mean - torch.exp(z_log_var)) / batch_size
 
-        epsilon = create_var(torch.randn(batch_size, self.latent_size / 2), False)
-        tree_vec = tree_mean + torch.exp(tree_log_var / 2) * epsilon
-        epsilon = create_var(torch.randn(batch_size, self.latent_size / 2), False)
-        mol_vec = mol_mean + torch.exp(mol_log_var / 2) * epsilon
+        epsilon = create_var(torch.randn(batch_size, self.latent_size // 2), False)
+        tree_vec = tree_mean + torch.exp(tree_log_var // 2) * epsilon
+        epsilon = create_var(torch.randn(batch_size, self.latent_size // 2), False)
+        mol_vec = mol_mean + torch.exp(mol_log_var // 2) * epsilon
         
         word_loss, topo_loss, word_acc, topo_acc = self.decoder(mol_batch, tree_vec)
         assm_loss, assm_acc = self.assm(mol_batch, mol_vec, tree_mess)
@@ -120,8 +120,8 @@ class JTNNVAE(nn.Module):
         batch_idx = create_var(torch.LongTensor(batch_idx))
         mol_vec = mol_vec.index_select(0, batch_idx)
 
-        mol_vec = mol_vec.view(-1, 1, self.latent_size / 2)
-        cand_vec = cand_vec.view(-1, self.latent_size / 2, 1)
+        mol_vec = mol_vec.view(-1, 1, self.latent_size // 2)
+        cand_vec = cand_vec.view(-1, self.latent_size // 2, 1)
         scores = torch.bmm(mol_vec, cand_vec).squeeze()
         
         cnt,tot,acc = 0,0,0
@@ -335,9 +335,128 @@ class DGLJTNNVAE(nn.Module):
         self.embedding = nn.Embedding(vocab.size(), hidden_size)
         self.mpn = DGLMPN(hidden_size, depth)
         self.jtnn = DGLJTNNEncoder(vocab, hidden_size, self.embedding)
+        self.decoder = DGLJTNNDecoder(
+                vocab, hidden_size, latent_size // 2, self.embedding)
+        self.jtmpn = DGLJTMPN(hidden_size, depth)
+
+        self.T_mean = nn.Linear(hidden_size, latent_size // 2)
+        self.T_var = nn.Linear(hidden_size, latent_size // 2)
+        self.G_mean = nn.Linear(hidden_size, latent_size // 2)
+        self.G_var = nn.Linear(hidden_size, latent_size // 2)
 
     def encode(self, mol_batch):
         smiles_batch = [mol_tree.smiles for mol_tree in mol_batch]
         mol_vec = self.mpn(mol2dgl(smiles_batch))
         # mol_batch is a junction tree
-        tree_mess, tree_vec = self.jtnn(mol_batch)
+        mol_tree_batch, tree_vec = self.jtnn(mol_batch)
+
+        return mol_tree_batch, tree_vec, mol_vec
+
+    def forward(self, mol_batch, beta=0):
+        batch_size = len(mol_batch)
+
+        mol_tree_batch, tree_vec, mol_vec = self.encode(mol_batch)
+
+        tree_mean = self.T_mean(tree_vec)
+        tree_log_var = -torch.abs(self.T_var(tree_vec))
+        mol_mean = self.G_mean(mol_vec)
+        mol_log_var = -torch.abs(self.G_var(mol_vec))
+
+        z_mean = torch.cat([tree_mean, mol_mean], dim=1)
+        z_log_var = torch.cat([tree_log_var, mol_log_var], dim=1)
+        kl_loss = -0.5 * torch.sum(1.0 + z_log_var - z_mean * z_mean - torch.exp(z_log_var)) / batch_size
+
+        epsilon = torch.randn(batch_size, self.latent_size // 2)
+        tree_vec = tree_mean + torch.exp(tree_log_var // 2) * epsilon
+        epsilon = torch.randn(batch_size, self.latent_size // 2)
+        mol_vec = mol_mean + torch.exp(mol_log_var // 2) * epsilon
+
+        word_loss, topo_loss, word_acc, topo_acc = self.decoder(mol_batch, tree_vec)
+        assm_loss, asm_acc = self.assm(mol_batch, mol_tree_batch, mol_vec)
+        stereo_loss, stereo_acc = self.stereo(mol_batch, mol_vec)
+
+        all_vec = torch.cat([tree_vec, mol_vec], dim=1)
+        loss = word_loss + topo_loss + assm_loss + 2 * stereo_loss + beta * kl_loss
+
+        return loss, kl_loss.item(), word_acc, topo_acc, assm_acc, stereo_acc
+
+    def assm(self, mol_batch, mol_tree_batch, mol_vec):
+        cands = []
+        batch_idx = []
+
+        for i, mol_tree in enumerate(mol_batch):
+            for node_id, node in mol_tree.nodes.items():
+                if node['is_leaf'] or len(node['cands']) == 1:
+                    continue
+                cands.extend([(cand, mol_tree, node_id) for cand in node['cand_mols']])
+                batch_idx.extend([i] * len(node['cands']))
+
+        cand_vec = self.jtmpn(cands, mol_tree_batch)
+        cand_vec = self.G_mean(cand_vec)
+
+        batch_idx = torch.LongTensor(batch_idx)
+        mol_vec = mol_vec[batch_idx]
+
+        mol_vec = mol_vec.view(-1, 1, self.latent_size // 2)
+        cand_vec = cand_vec.view(-1, self.latent_size // 2, 1)
+        scores = (mol_vec @ cand_vec)[:, 0, 0]
+
+        cnt, tot, acc = 0, 0, 0
+        all_loss = []
+        for i, mol_tree in enumerate(mol_batch):
+            comp_nodes = [node_id for node_id, node in mol_tree.nodes.items()
+                          if len(node['cands']) > 1 and not node['is_leaf']]
+            cnt += len(comp_nodes)
+            # segmented accuracy and cross entropy
+            for node in comp_nodes:
+                label = node['cands'].index(node['label'])
+                ncand = len(node['cands'])
+                cur_score = scores[tot:tot+ncand]
+                tot += ncand
+
+                if cur_score[label].item() >= cur_score.max().item():
+                    acc += 1
+
+                label = torch.LongTensor([label])
+                all_loss.append(
+                    F.cross_entropy(cur_score.view(1, -1), label, size_average=False))
+
+        all_loss = sum(all_loss) / len(all_loss)
+        return all_loss, acc / cnt
+
+    def stereo(self, mol_batch, mol_vec):
+        stereo_cands, batch_idx = [], []
+        labels = []
+        for i, mol_tree in enumerate(mol_batch):
+            cands = mol_tree.stereo_cands
+            if len(cands) == 1:
+                continue
+            if mol_tree.smiles3D not in cands:
+                cands.append(mol_tree.smiles3D)
+            stereo_cands.extend(cands)
+            batch_idx.extend([i] * len(cands))
+            labels.append((cands.index(mol_tree.smiles3D), len(cands)))
+
+        if len(labels) == 0:
+            # Only one stereoisomer exists; do nothing
+            return torch.tensor(0), 1.
+
+        batch_idx = torch.LongTensor(batch_idx)
+        stereo_cands = self.mpn(mol2graph(stereo_cands))
+        stereo_cands = self.G_mean(stereo_cands)
+        stereo_labels = mol_vec[batch_idx]
+        scores = F.cosine_similarity(stereo_cands, stereo_labels)
+
+        st, acc = 0, 0
+        all_loss = []
+        for label, le in labels:
+            cur_scores = scores[st:st+le]
+            if cur_scores.data[label].item() >= cur_scores.max().item():
+                acc += 1
+            label = torch.LongTensor(label)
+            all_loss.append(
+                F.cross_entropy(cur_scores.view(1, -1), label, size_average=False))
+            st += le
+
+        all_loss = sum(all_loss) / len(labels)
+        return all_loss, acc / len(labels)
