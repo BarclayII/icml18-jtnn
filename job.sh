704551648a5f195320c89003976c66169b4d30e2
Gan Quan <coin2028@hotmail.com>
2018-09-19 23:27:02 -0400

    shell scripts

---
+++ job.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=/home/gq/ML/icml18-jtnn::/opt/RDKit/lib64/python3.6/site-packages:/opt/RDKit/lib64/python3.6/site-packages python3 molvae/vaetrain.py -t data/trainmini.txt -v data/vocab.txt -s .
